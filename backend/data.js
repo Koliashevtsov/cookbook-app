5914c9ae36597faf67464221dd213e0fa1b0fb23
Koliashevtsov <39794420+Koliashevtsov@users.noreply.github.com>
2020-05-26 17:48:26 +0300

    Update data.js

--- backend/data.js
+++ backend/data.js
@@ -1,11 +1,1 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const dataSchema = new Schema(
-    {
-        id: Number,
-        message: String
-    }
-)
-
-module.exports = mongoose.model("Data", dataSchema);
